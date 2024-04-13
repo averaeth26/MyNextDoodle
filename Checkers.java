7fd4d95440eaffa6d264918170dd164b4361ebb7
MrScorcher1 <eavera@theaveras.net>
2024-04-13 01:10:14 -0400

    Finished piece movement, not including jumping.

--- Checkers.java
+++ Checkers.java
@@ -2,19 +2,23 @@ import doodlepad.*;
 import java.util.ArrayList;
 
 public class Checkers {
-    int screenWidth = 1200;
-    int screenHeight = 1200;
+    int screenWidth = 800;
+    int screenHeight = 800;
     int numTiles = 8;
+    int selectedPieceRow;
+    int selectedPieceCol;
+    Pad p = new Pad(screenWidth, screenHeight);
     int squareWidth = screenWidth/numTiles;;
     int squareHeight= screenHeight/numTiles;;
-    ArrayList<Oval> validMoves;
+    ArrayList<Oval> validMoves = new ArrayList<Oval>();
     ArrayList<Man> pieces = new ArrayList<Man>();
     int[][] grid = new int[numTiles][numTiles];
     
     public Checkers() {
         drawBackground();
         generateGrid();
-        generatePieces();
+        generatePieceGrid();
+        drawPieces();
     }
 
     public void drawBackground() {
@@ -41,29 +45,34 @@ public class Checkers {
     }
 
     public void onPieceClicked(Shape shp, double x, double y, int button) {
-        shp.setFillColor(0, 200, 200);
-        // calculateValidMoves(shp.getCenter().getX(), shp.getCenter().getY());
-    }  
+        drawPieces();
+        calculateValidMoves(shp.getCenter().getX(), shp.getCenter().getY());
+    }
+
+    public void selectMoveChoice(Shape shp, double x, double y, int button) {
+        grid[(int)shp.getCenter().getX()/squareHeight][(int)shp.getCenter().getY()/squareHeight] = 1;
+        grid[selectedPieceCol][selectedPieceRow] = 0;
+        drawPieces();
+    } 
     
+    // TODO: Need to include jumping over other pieces here.
     public void calculateValidMoves(double pieceX, double pieceY) {
-        int currentRow = (int)pieceY/squareHeight;
-        int currentCol = (int)pieceX/squareWidth;
-        System.out.println(currentRow);
-        validMoves = new ArrayList<Oval>();
+        selectedPieceRow = (int)pieceY/squareHeight;
+        selectedPieceCol = (int)pieceX/squareWidth;
         for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; r < grid[r].length; c++) {
-                if (Math.abs(currentRow-r) == 1 && Math.abs(currentCol-c) == 1) {
-                    if (grid[r][c] == 0) {
-                        validMoves.add(new Oval(c*squareWidth, r*squareWidth, squareWidth/3, squareWidth/3));
-                        validMoves.get(validMoves.size()-1).setFillColor(128, 128, 128);
+            for (int c = 0; c < grid[r].length; c++) {
+                if (Math.abs(selectedPieceRow-r) == 1 && Math.abs(selectedPieceCol-c) == 1) {
+                    if (grid[c][r] == 0) {
+                        validMoves.add(new Oval(c*squareHeight + squareHeight/3, r*squareWidth + squareWidth/3, squareWidth/3, squareHeight/3));
+                        validMoves.get(validMoves.size()-1).setFillColor(128, 128, 128, 128);
+                        validMoves.get(validMoves.size()-1).setMouseClickedHandler(this::selectMoveChoice);
                     }
                 }
             }
         }
     }
 
-    public ArrayList<Man> generatePieces() {
-        ArrayList<Man> currPieces = new ArrayList<Man>();
+    public void generatePieceGrid() {
         for (int i = 0; i < numTiles; i++) {
             System.out.println(((double)numTiles/2.0) - (double)i);
             for (int j = 0; j < numTiles; j++) {
@@ -71,13 +80,27 @@ public class Checkers {
                     continue;
                 }
                 if ((i+j)%2 != 0) {
-                    currPieces.add(new Man(i, j, squareWidth, squareHeight));
                     grid[i][j] = 1;
-                    currPieces.get(currPieces.size()-1).getHitbox().setMousePressedHandler(this::onPieceClicked);
                 }
             }
         }
+    }
+
+    public ArrayList<Man> drawPieces() {
+        drawBackground();
+        ArrayList<Man> currPieces = new ArrayList<Man>();
+        for (int i = 0; i < numTiles; i++) {
+            for (int j = 0; j < numTiles; j++) {
+                if (grid[i][j] == 1) {
+                    currPieces.add(new Man(i, j, squareWidth, squareHeight));
+                    currPieces.get(currPieces.size()-1).getHitbox().setMousePressedHandler(this::onPieceClicked);
+                }
+
+            }
+        }
         return currPieces;
+
+
     }
 
 }
--- Tester.java
+++ Tester.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class Tester {
     public static void main(String[] args) {
-        Pad p = new Pad(1200, 1200);
         Checkers board = new Checkers();
     }
 }
